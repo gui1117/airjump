eceb85b79e050f751e49fd2e5d606efceb08c0ed
thiolliere <guillaume.thiolliere@opmbx.org>
2017-05-21 14:44:59 +0200

    update map

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,7 @@
 extern crate svgparser;
 
 use self::svgparser::svg;
+use self::svgparser::Tokenize;
 use physics::{Body, Shape};
 use OkOrExit;
 
@@ -14,6 +15,7 @@ const MAP_FILE: &'static str = "map.svg";
 enum Error {
     Io(::std::io::Error),
     Svg(svgparser::Error),
+    ParseFloat(::std::num::ParseFloatError),
 }
 impl From<::std::io::Error> for Error {
     fn from(err: ::std::io::Error) -> Error {
@@ -25,34 +27,40 @@ impl From<svgparser::Error> for Error {
         Error::Svg(err)
     }
 }
+impl From<::std::num::ParseFloatError> for Error {
+    fn from(err: ::std::num::ParseFloatError) -> Error {
+        Error::ParseFloat(err)
+    }
+}
 impl ::std::fmt::Display for Error {
     fn fmt(&self, fmt: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
         use self::Error::*;
         match *self {
             Io(ref e) => write!(fmt, "file `{}`: io error: {}", MAP_FILE, e),
             Svg(ref e) => write!(fmt, "file `{}`: svg parser error: {:?}", MAP_FILE, e),
+            ParseFloat(ref e) => write!(fmt, "file `{}`: svg parser float error: {:?}", MAP_FILE, e),
         }
     }
 }
 
 #[cfg(feature = "exclude_all")]
-fn read_map_file() -> Result<Vec<u8>, Error> {
+fn read_map_file() -> Result<String, Error> {
     use std::fs::File;
     use std::io::Read;
-    let mut map = Vec::new();
-    File::open(MAP_FILE)?.read_to_end(&mut map)?;
+    let mut map = String::new();
+    File::open(MAP_FILE)?.read_to_string(&mut map)?;
     Ok(map)
 }
 
 #[cfg(not(feature = "exclude_all"))]
-fn read_map_file() -> Result<&'static [u8], Error> {
-    Ok(include_bytes!("../map.svg"))
+fn read_map_file() -> Result<&'static str, Error> {
+    Ok(include_str!("../map.svg"))
 }
 
 fn load_map() -> Result<Map, Error> {
     let text = read_map_file()?;
 
-    let mut parser = svg::Tokenizer::new(&text);
+    let mut parser = svg::Tokenizer::from_str(&text);
 
     let mut bodies = Vec::new();
 
@@ -65,15 +73,14 @@ fn load_map() -> Result<Map, Error> {
     let mut rect: Option<(Option<f64>,Option<f64>,Option<f64>,Option<f64>)> = None;
 
     loop {
+        use self::svgparser::ElementId;
+        use self::svgparser::AttributeId;
+        use self::svgparser::svg::ElementEnd::Empty;
+
         match parser.parse_next()? {
-            svg::Token::ElementStart(name) => {
-                if name == b"circle" {
-                    circle = Some((false, None, None, None));
-                } else if name == b"rect" {
-                    rect = Some((None, None, None, None));
-                }
-            },
-            svg::Token::ElementEnd(_) => {
+            svg::Token::SvgElementStart(ElementId::Circle) => circle = Some((false, None, None, None)),
+            svg::Token::SvgElementStart(ElementId::Rect) => rect = Some((None, None, None, None)),
+            svg::Token::ElementEnd(Empty) => {
                 if let Some(circle) = circle.take() {
                     match circle {
                         (true, Some(x), Some(y), _) => {
@@ -94,49 +101,48 @@ fn load_map() -> Result<Map, Error> {
                             pos: [x+w/2., y-h/2.],
                             shape: Shape::Rectangle(w, h),
                         }),
-                        _ => println!("WARGNING: svg map incomplete circle definition"),
+                        _ => println!("WARGNING: svg map incomplete rect definition"),
                     }
                 }
             },
-            svg::Token::Attribute(b"id", value) => {
+            svg::Token::SvgAttribute(AttributeId::Id, value) => {
                 if let Some(ref mut circle) = circle {
-                    let d = b"start";
-                    circle.0 = value.slice_next_raw(d.len()) == d;
+                    circle.0 = value.slice() == "start";
                 }
             },
-            svg::Token::Attribute(b"cx", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::Cx, value) => {
                 if let Some(ref mut circle) = circle {
-                    circle.1 = Some(value.parse_number()?);
+                    circle.1 = Some(value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"cy", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::Cy, value) => {
                 if let Some(ref mut circle) = circle {
-                    circle.2 = Some(-value.parse_number()?);
+                    circle.2 = Some(-value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"r", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::R, value) => {
                 if let Some(ref mut circle) = circle {
-                    circle.3 = Some(value.parse_number()?);
+                    circle.3 = Some(value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"x", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::X, value) => {
                 if let Some(ref mut rect) = rect {
-                    rect.0 = Some(value.parse_number()?);
+                    rect.0 = Some(value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"y", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::Y, value) => {
                 if let Some(ref mut rect) = rect {
-                    rect.1 = Some(-value.parse_number()?);
+                    rect.1 = Some(-value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"width", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::Width, value) => {
                 if let Some(ref mut rect) = rect {
-                    rect.2 = Some(value.parse_number()?);
+                    rect.2 = Some(value.slice().parse()?);
                 }
             },
-            svg::Token::Attribute(b"height", mut value) => {
+            svg::Token::SvgAttribute(AttributeId::Height, value) => {
                 if let Some(ref mut rect) = rect {
-                    rect.3 = Some(value.parse_number()?);
+                    rect.3 = Some(value.slice().parse()?);
                 }
             },
             svg::Token::EndOfStream => break,
@@ -147,7 +153,7 @@ fn load_map() -> Result<Map, Error> {
     Ok(Map {
         bodies: bodies,
         start: start.unwrap_or_else(|| {
-            println!("WARGNING: svg map incomplete circle definition");
+            println!("WARGNING: svg map no start definition");
             [0., 0.]
         }),
     })
